20d5e8e18a08f0dcdf88c033d18ff7850ac223fa
Erik Santos <113066264+erkss@users.noreply.github.com>
2023-09-11 15:45:23 -0300

    Update Main.java

--- Desafios/Animal/Main.java
+++ Desafios/Animal/Main.java
@@ -12,52 +12,29 @@ public class Main {
         palavra2 = scan.next();
         palavra3 = scan.next();
 
-        if(palavra1.equalsIgnoreCase("vertebrado")) {
-            if(palavra2.equalsIgnoreCase("ave"))
-                if(palavra3.equalsIgnoreCase("carnivoro"))
-                    System.out.println("aguia");
-        }
-
-        if(palavra1.equalsIgnoreCase("vertebrado")) {
-            if(palavra2.equalsIgnoreCase("ave"))
-                if(palavra3.equalsIgnoreCase("onivoro"))
-                    System.out.println("pomba");
-        }
-
-        if(palavra1.equalsIgnoreCase("vertebrado")) {
-            if(palavra2.equalsIgnoreCase("mamifero"))
-                if(palavra3.equalsIgnoreCase("onivoro"))
-                    System.out.println("homem");
-        }
-
-        if(palavra1.equalsIgnoreCase("vertebrado")) {
-            if(palavra2.equalsIgnoreCase("mamifero"))
-                if(palavra3.equalsIgnoreCase("herbivoro"))
-                    System.out.println("vaca");
-        }
-
-        if(palavra1.equalsIgnoreCase("invertebrado")) {
-            if(palavra2.equalsIgnoreCase("inseto"))
-                if(palavra3.equalsIgnoreCase("hematofago"))
-                    System.out.println("pulga");
-        }
-
-        if(palavra1.equalsIgnoreCase("invertebrado")) {
-            if(palavra2.equalsIgnoreCase("inseto"))
-                if(palavra3.equalsIgnoreCase("herbivoro"))
-                    System.out.println("lagarta");
-        }
-
-        if(palavra1.equalsIgnoreCase("invertebrado")) {
-            if(palavra2.equalsIgnoreCase("anelideo"))
-                if(palavra3.equalsIgnoreCase("hematofago"))
-                    System.out.println("sanguessuga");
-        }
-
-        if(palavra1.equalsIgnoreCase("invertebrado")) {
-            if(palavra2.equalsIgnoreCase("anelideo"))
-                if(palavra3.equalsIgnoreCase("onivoro"))
-                    System.out.println("minhoca");
+        if (palavra1.equalsIgnoreCase("vertebrado") && palavra2.equalsIgnoreCase("ave") && palavra3.equalsIgnoreCase("carnivoro")) {
+            System.out.println("aguia");
+        } 
+        else if (palavra1.equalsIgnoreCase("vertebrado") && palavra2.equalsIgnoreCase("ave") && palavra3.equalsIgnoreCase("onivoro")) {
+            System.out.println("pomba");
+        } 
+        else if (palavra1.equalsIgnoreCase("vertebrado") && palavra2.equalsIgnoreCase("mamifero") && palavra3.equalsIgnoreCase("onivoro")) {
+            System.out.println("homem");
+        } 
+        else if (palavra1.equalsIgnoreCase("vertebrado") && palavra2.equalsIgnoreCase("mamifero") && palavra3.equalsIgnoreCase("herbivoro")) {
+            System.out.println("vaca");
+        } 
+        else if (palavra1.equalsIgnoreCase("invertebrado") && palavra2.equalsIgnoreCase("inseto") && palavra3.equalsIgnoreCase("hematofago")) {
+            System.out.println("pulga");
+        } 
+        else if (palavra1.equalsIgnoreCase("invertebrado") && palavra2.equalsIgnoreCase("inseto") && palavra3.equalsIgnoreCase("herbivoro")) {
+            System.out.println("lagarta");
+        } 
+        else if (palavra1.equalsIgnoreCase("invertebrado") && palavra2.equalsIgnoreCase("anelideo") && palavra3.equalsIgnoreCase("hematofago")) {
+            System.out.println("sanguessuga");
+        } 
+        else if (palavra1.equalsIgnoreCase("invertebrado") && palavra2.equalsIgnoreCase("anelideo") && palavra3.equalsIgnoreCase("onivoro")) {
+            System.out.println("minhoca");
         }
     }
 }
